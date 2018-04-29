677c45483b798168f377a1e3179a0bf97435a565
JianLiHoo <hoojianli@gmail.com>
2018-04-29 00:53:42 -0700

    Add files via upload

---
+++ Controller.ino
@@ -0,0 +1,51 @@
+#include <SoftwareSerial.h>
+
+SoftwareSerial mySerial(11, 12); // RX, TX
+
+int joyLy;
+int joyRy;
+boolean left;
+boolean right;
+
+#define leftBtn 5
+#define rightBtn 4
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(leftBtn, INPUT_PULLUP);
+  pinMode(rightBtn, INPUT_PULLUP);
+
+  mySerial.begin(38400);
+}
+
+void loop() {
+  String bluetooth = "";
+  left = digitalRead(leftBtn);
+  right = digitalRead(rightBtn);
+  if (left == LOW){
+    bluetooth = bluetooth + '1';
+  }
+  else{
+    bluetooth = bluetooth + '0';
+  }
+  
+  if (right == LOW){
+    bluetooth = bluetooth + '1';
+  }
+  else{
+    bluetooth = bluetooth + '0';
+  }
+
+  joyLy = analogRead(A2);
+  joyRy = 1023 - analogRead(A4); //flip the values
+  //joyLy = map(joyLy,0,1023,0,255);  //conversion on tank
+  //joyRy = map(joyRy,0,1023,0,255);
+
+  bluetooth = bluetooth + joyLy;
+  bluetooth = bluetooth + ',';
+  bluetooth = bluetooth + joyRy;
+  //bluetooth = bluetooth + 'c';
+  mySerial.println(bluetooth);  // \n used for cancel
+  delay(10);
+
+}
---
+++ Tank.ino
@@ -0,0 +1,136 @@
+#include <SoftwareSerial.h>
+
+SoftwareSerial mySerial(11, 12); // RX, TX
+
+int pos = 0; // button pos
+String bluetooth; //val from bluetooth
+char leftButton;
+char rightButton;
+
+//Motor A
+
+int in1 = 5;
+int in2 = 6;
+
+int motorAVal;
+
+//Motor B
+
+int in3 = 9;
+int in4 = 10;
+
+int motorBVal;
+
+void setup() {
+  Serial.begin(38400);
+  /*
+  pinMode(in1, OUTPUT);
+  pinMode(in2, OUTPUT);
+  pinMode(in3, OUTPUT);
+  pinMode(in4, OUTPUT);
+  */
+  mySerial.begin(38400);
+}
+/*
+void loop() { // run over and over
+  if (mySerial.available()) {
+    Serial.write(mySerial.read());
+  }
+  if (Serial.available()) {
+    mySerial.write(Serial.read());
+  }
+}
+*/
+
+void loop() {
+  if (mySerial.available()){
+    bluetooth = mySerial.readStringUntil('\n');
+
+    //Parsing
+
+    leftButton = bluetooth.charAt(0);
+    rightButton = bluetooth.charAt(1);
+    bluetooth.remove(0,2);
+    //Serial.println(bluetooth);
+    motorAVal=bluetooth.substring(0,bluetooth.indexOf(',')).toInt();
+    motorBVal=bluetooth.substring(bluetooth.indexOf(',')+1).toInt();
+    
+    //Serial.println(motorBVal);
+    motorAVal=map(motorAVal,0,1023,-255,255);
+    motorBVal=map(motorBVal,0,1023,-255,255);
+
+    
+
+    //--------Direction-----------
+    if((motorAVal<-10)&&(motorBVal<-10)){ //A Rev, B Rev
+      analogWrite(in1,0);
+      analogWrite(in2,abs(motorAVal));
+
+      analogWrite(in3,0);
+      analogWrite(in4,abs(motorBVal));
+    }
+    else if((motorAVal>10)&&(motorBVal>10)){  //A Fwd, B Fwd
+      analogWrite(in1,abs(motorAVal));
+      analogWrite(in2,0);
+
+      analogWrite(in3,abs(motorBVal));
+      analogWrite(in4,0);
+    }
+    else if((motorAVal>10)&&(motorBVal<-10)){  //A Fwd, B Rev
+      analogWrite(in1,abs(motorAVal));
+      analogWrite(in2,0);
+
+      analogWrite(in3,0);
+      analogWrite(in4,abs(motorBVal));
+    }
+    else if((motorAVal<-10)&&(motorBVal>10)){  //A Rev, B Fwd
+      analogWrite(in1,0);
+      analogWrite(in2,abs(motorAVal));
+      
+      analogWrite(in3,abs(motorBVal));
+      analogWrite(in4,0);
+    }
+    else if((motorAVal<-10)&&((motorBVal>-10)&&(motorBVal<10))){  //A Rev, B Stop
+      analogWrite(in1,0);
+      analogWrite(in2,abs(motorAVal));
+      
+      analogWrite(in3,0);
+      analogWrite(in4,0);
+    }
+    else if((motorAVal>10)&&((motorBVal>-10)&&(motorBVal<10))){  //A Fwd, B Stop
+      analogWrite(in1,abs(motorAVal));
+      analogWrite(in2,0);
+      
+      analogWrite(in3,0);
+      analogWrite(in4,0);
+    }
+    else if((motorBVal<-10)&&((motorAVal>-10)&&(motorAVal<10))){  //A Stop, B Rev
+      analogWrite(in1,0);
+      analogWrite(in2,0);
+      
+      analogWrite(in3,0);
+      analogWrite(in4,abs(motorBVal));
+    }
+    else if((motorBVal>10)&&((motorAVal>-10)&&(motorAVal<10))){  //A Stop, B Fwd
+      analogWrite(in1,0);
+      analogWrite(in2,0);
+      
+      analogWrite(in3,abs(motorBVal));
+      analogWrite(in4,0);
+    }
+    
+    else{ //All stop
+      analogWrite(in1,0);
+      analogWrite(in2,0);
+      
+      analogWrite(in3,0);
+      analogWrite(in4,0);
+    }
+
+    
+    Serial.print(motorAVal);
+    Serial.println(motorBVal);
+
+  }
+}
+
