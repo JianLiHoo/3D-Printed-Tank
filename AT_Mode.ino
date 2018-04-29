76b3d3618769131a9a140126c6c5d771851594a3
JianLiHoo <hoojianli@gmail.com>
2018-04-29 00:58:01 -0700

    Add files via upload

---
+++ AT_Mode.ino
@@ -0,0 +1,56 @@
+//  Uses hardware serial to talk to the host computer and software serial 
+//  for communication with the Bluetooth module
+//  Intended for Bluetooth devices that require line end characters "\r\n"
+//
+//  Pins
+//  Arduino 5V out TO BT VCC
+//  Arduino GND to BT GND
+//  Arduino D9 to BT RX through a voltage divider
+//  Arduino D8 BT TX (no need voltage divider)
+//
+//  When a command is entered in the serial monitor on the computer 
+//  the Arduino will relay it to the bluetooth module and display the result.
+
+#include <SoftwareSerial.h>
+SoftwareSerial BTserial(11,12); // RX | TX
+ 
+const long baudRate = 38400; 
+char c=' ';
+boolean NL = true;
+ 
+void setup() 
+{
+    Serial.begin(38400);
+    Serial.print("Sketch:   ");   Serial.println(__FILE__);
+    Serial.print("Uploaded: ");   Serial.println(__DATE__);
+    Serial.println(" ");
+ 
+    BTserial.begin(baudRate);  
+    Serial.print("BTserial started at "); Serial.println(baudRate);
+    Serial.println(" ");
+}
+ 
+void loop()
+{
+ 
+    // Read from the Bluetooth module and send to the Arduino Serial Monitor
+    if (BTserial.available())
+    {
+        c = BTserial.read();
+        Serial.write(c);
+    }
+ 
+ 
+    // Read from the Serial Monitor and send to the Bluetooth module
+    if (Serial.available())
+    {
+        c = Serial.read();
+        BTserial.write(c);   
+ 
+        // Echo the user input to the main window. The ">" character indicates the user entered text.
+        if (NL) { Serial.print(">");  NL = false; }
+        Serial.write(c);
+        if (c==10) { NL = true; }
+    }
+ 
+}
